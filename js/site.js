fdfd0e760b3c30fbf5e5f1e8e1c839fab2e7d613
truRebel <KiddDiop@gmail.com>
2021-08-18 16:16:45 -0700

    Rewind Complete
    MVC Complete

--- js/site.js
+++ js/site.js
@@ -1,5 +1,36 @@
-function HelloWorld() {
+//Get the string from the page(UserInput)
+//controller function
+function getValue() {
 
-    //Call Hello World
-    alert("Hello World");
+    
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+
+    
+}
+
+//Reverse the string
+//logic funtion
+function reverseString(userString) {
+    
+    let revString = [];
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index>=0; index--) {
+        
+        revString += userString[index];
+        
+    }
+    
+    return revString;
+}
+//Display messange and the reversed string
+//view function
+function displayString(revString) {
+
+    //write to the page
+    document.getElementById("msg").innerHTML = `${revString}`;
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
